537c926d709f3d4007562f54625d1c47835aeb19
0xInuarashi <0xInuarashi@cypherlabz.io>
2024-02-28 03:40:55 +0700

    shits complexito man

--- src/tokens/ERC404/SFT418Linked.sol
+++ src/tokens/ERC404/SFT418Linked.sol
@@ -18,9 +18,17 @@ interface ISF418Primary {
     function name() external view returns (string memory);
     function symbol() external view returns (string memory);
 
-    function _ownerOf(uint256 tokenId_) external view returns (address);
-    function _balanceOf(address wallet_) external view returns (uint256);
+    function _NFT_ownerOf(uint256 tokenId_) external view returns (address);
+    function _NFT_balanceOf(address wallet_) external view returns (uint256);
+    function _NFT_getApproved(uint256 tokenId_) external view returns (address);
+    function _NFT_isApprovedForAll(address spender_, address operator_) external view returns (bool);
 
+    function _NFT_approve(address spender_, uint256 tokenId_, address msgSender_) external;
+    function _NFT_setApprovalForAll(address operator_, bool approved_, address msgSender_) external;
+    function _NFT_transferFrom(address from_, address to_, uint256 tokenId_) external;
+
+    function _NFT_mint(address to_, uint256 amount_) external;
+    function _NFT_burn(address from_, uint256 amount_) external;
 }
 
 abstract contract SFT418Pair {
@@ -33,6 +41,18 @@ abstract contract SFT418Pair {
     event Approval(address indexed owner, address indexed spender, uint256 indexed id);
     event ApprovalForAll(address indexed owner, address indexed operator, bool approved);
 
+    // keccak256(bytes("Transfer(address,address,uint256)"))
+    bytes32 internal constant _TRANSFER_EVENT_SIGNATURE = 
+        0xddf252ad1be2c89b69c2b068fc378daa952ba7f163c4a11628f55a4df523b3ef;
+    
+    // keccak256(bytes("Approval(address,address,uint256)"))
+    bytes32 internal constant _APPROVAL_EVENT_SIGNATURE =
+        0x8c5be1e5ebec7d5bd14f71427d1e84f3dd0314c0f7b2291e5b200ac8c7c3b925;
+    
+    // keccak256(bytes("ApprovalForAll(address,address,bool)"))
+    bytes32 internal constant _APPROVAL_FOR_ALL_EVENT_SIGNATURE = 
+        0x17307eab39ab6107e8899845ad3d59bd9653f200f220920489ca2b5937696c31;
+
     /////////////////////////////////
     // SFT418Primary Connector //////
     /////////////////////////////////
@@ -44,20 +64,222 @@ abstract contract SFT418Pair {
     address internal _deployer;
 
     /////////////////////////////////
-    // SFT418 Interface Functions ///
+    // SFT418 Interface Reads ///////
     /////////////////////////////////
 
     function ownerOf(uint256 tokenId_) public virtual view returns (address) {
-        return SFT418._ownerOf(tokenId_);
+        return SFT418._NFT_ownerOf(tokenId_);
     }
 
     function balanceOf(address wallet_) public virtual view returns (uint256) {
-        return SFT418._balanceOf(wallet_);
+        return SFT418._NFT_balanceOf(wallet_);
+    }
+
+    function getApproved(uint256 tokenId_) public virtual view returns (address) {
+        return SFT418._NFT_getApproved(tokenId_);
+    }
+
+    function isApprovedForAll(address spender_, address operator_) public virtual view returns (bool) {
+        return SFT418._NFT_isApprovedForAll(spender_, operator_);
+    }
+
+    /////////////////////////////////
+    // SFT418 Interface Writes //////
+    /////////////////////////////////
+
+    function approve(address spender_, uint256 tokenId_) public virtual {
+        SFT418._NFT_approve(spender_, tokenId_, msg.sender);
+    }
+
+    function setApprovalForAll(address operator_, bool approved_) public virtual {
+        SFT418._NFT_setApprovalForAll(operator_, approved_, msg.sender);
+    }
+
+    function transferFrom(address from_, address to_, uint256 tokenId_) public virtual {
+        SFT418._NFT_transferFrom(from_, to_, tokenId_);
+    }
+
+    /////////////////////////////////
+    // SFT418 Interface Writes //////
+    /////////////////////////////////
+
+    function _mint(address to_, uint256 amount_) internal virtual {
+        SFT418._NFT_mint(to_, amount_);
+    }
+
+    function _burn(address from_, uint256 amount_) internal virtual {
+        SFT418._NFT_burn(from_, amount_);
     }
 
     /////////////////////////////////
     // ERC721 Native Functions //////
     /////////////////////////////////
 
+    function _checkOnERC721Received(address from_, address to_, uint256 tokenId_, bytes memory data_) internal virtual {
+        require(
+            to_.code.length == 0 ||
+                ERC721TokenReceiver(to_).onERC721Received(from_, to_, tokenId_, data_) ==
+                ERC721TokenReceiver.onERC721Received.selector,
+            "SFT418: _checkOnERC721Received not ERC721 receiver"
+        );
+    }
+
+    function safeTransferFrom(address from_, address to_, uint256 tokenId_, bytes calldata data_) public virtual {
+        SFT418._NFT_transferFrom(from_, to_, tokenId_);
+        _checkOnERC721Received(from_, to_, tokenId_, data_);
+    }
+
+    function safeTransferFrom(address from_, address to_, uint256 tokenId_) public virtual {
+        SFT418._NFT_transferFrom(from_, to_, tokenId_);
+        _checkOnERC721Received(from_, to_, tokenId_, "");
+    }
+
     function tokenURI(uint256 tokenId_) public virtual view returns (string memory);
+
+    /////////////////////////////////
+    // ERC165 Interface /////////////
+    /////////////////////////////////
+
+    function supportsInterface(bytes4 interface_) public view virtual returns (bool) {
+        return
+            interface_ == 0x01ffc9a7 || // ERC165 Interface ID for ERC165 `supportsInterface(bytes4)`
+            interface_ == 0x80ac58cd || // ERC165 Interface ID for ERC721
+            interface_ == 0x5b5e139f;   // ERC165 Interface ID for ERC721Metadata
+    }
+
+    /////////////////////////////////
+    // Fallback SFT418 Interface ////
+    /////////////////////////////////
+    // YUL snippets inspired by DN404 / Solady 
+
+    function _calldataload(uint256 offset_) private pure returns (uint256 _value) {
+        assembly {
+            _value := calldataload(offset_)
+        }
+    }
+
+    modifier sft418fallback() virtual {
+        // Grab the first bytes32 of calldata and right shift bytes28 resulting in bytes4 selector
+        bytes4 _fnSelector = bytes4(bytes32(_calldataload(0x00) >> 224)); 
+
+        // Load the address of the SFT418 pair
+        address _pairAddress = address(SFT418);
+
+        // ASM VERSION
+        // "emitTransfers(address,address,uint256[])" >> "0xc9063eae"
+        if (_fnSelector == 0xc9063eae) {
+            require(msg.sender == _pairAddress, "SFT418Pair: fallback() emitTransfers not from pair");
+            assembly {
+                let from_ := calldataload(0x04) // load from_ address from first 32 bytes of calldata arguments
+                let to_ := calldataload(0x24)  // load to_ address from second 32 bytes of calldata args
+                let o := add(0x24, calldataload(0x44)) // get the offset of tokenIds_ array
+                let end := add(o, shl(5, calldataload(sub(o, 0x20)))) // get the end of tokenIds_ array
+                for {} iszero(eq(o, end)) { o:= add(0x20, o) } { // yul loop
+                    // emit NFT transfer logs using log4 (3 indexed values)
+                    log4(codesize(), 0x00, _TRANSFER_EVENT_SIGNATURE, from_, to_, calldataload(o))
+                }
+
+                // return true
+                mstore(0x00, 0x01)
+                return(0x00, 0x20)
+            }
+        }
+
+        // "emitTransfer(address,address,uint256)" >> "0x23de6651"
+        if (_fnSelector == 0x23de6651) {
+            require(msg.sender == _pairAddress, "SFT418Pair: fallback() emitTransfer not from pair");
+            assembly {
+                let from_ := calldataload(0x04)
+                let to_ := calldataload(0x24)
+                let tokenId_ := calldataload(0x44)
+                log4(codesize(), 0x00, _TRANSFER_EVENT_SIGNATURE, from_, to_, tokenId_)
+                mstore(0x00, 0x01)
+                return(0x00, 0x20)
+            }
+        }
+
+        // "emitApproval(address,address,uint256)" >> "0x5687f2b8"
+        if (_fnSelector == 0x5687f2b8) {
+            require(msg.sender == _pairAddress, "SFT418Pair: fallback() emitApproval not from pair");
+            assembly {
+                let owner_ := calldataload(0x04)
+                let spender_ := calldataload(0x24)
+                let tokenId_ := calldataload(0x44)
+                log4(codesize(), 0x00, _APPROVAL_EVENT_SIGNATURE, owner_, spender_, tokenId_)
+                mstore(0x00, 0x01)
+                return(0x00, 0x20)
+            }
+        }
+
+        // "emitSetApprovalForAll(address,address,bool)" >> "0xfb5a1525"
+        if (_fnSelector == 0xfb5a1525) {
+            require(msg.sender == _pairAddress, "SFT418Pair: fallback() emitSetApprovalForAll not from pair");
+            assembly {
+                let owner_ := calldataload(0x04)
+                let operator_ := calldataload(0x24)
+                let approved_ := calldataload(0x44)
+                
+                // load approved_ and emit it as non-indexed event data
+                mstore(0x00, approved_) 
+                log3(0x00, 0x20, _APPROVAL_FOR_ALL_EVENT_SIGNATURE, owner_, operator_)
+                
+                // replace approved_ with 1 to return true afterwards
+                mstore(0x00, 0x01)
+                return(0x00, 0x20)
+            }
+        }
+
+        // // ABSTRACTED VERSION
+        // // "emitTransfers(address,address,uint256[])" >> "0xc9063eae"
+        // if (_fnSelector == ISFT418Pair.emitTransfers.selector) {
+        //     // receive msg.sender and make sure it's _pairAddress only
+        //     require(msg.sender == _pairAddress, "SFT418Pair: fallback() emitTransfers not from pair");
+
+        //     // grab calldata length
+        //     uint256 _calldataLength = msg.data.length;
+        //     uint256 _argumentsLength = _calldataLength - 4;
+
+        //     // the minimum amount of length for an (address,address,uint256[]) is 32+3 (96)
+        //     require(_argumentsLength >= 96, "SFT418Pair: calldata length invalid");
+
+        // }
+
+        // // "emitTransfer(address,address,uint256)" >> "0x23de6651"
+        // if (_fnSelector == ISFT418Pair.emitTransfer.selector) {
+        //     require(msg.sender == _pairAddress, "SFT418Pair: fallback() emitTransfers not from pair");
+
+        // }
+
+        // // "emitApproval(address,address,uint256)" >> "0x5687f2b8"
+        // if (_fnSelector == ISFT418Pair.emitApproval.selector) {
+        //     require(msg.sender == _pairAddress, "SFT418Pair: fallback() emitTransfers not from pair");
+
+        // }
+
+        // // "emitSetApprovalForAll(address,address,bool)" >> "0xfb5a1525"
+        // if (_fnSelector == ISFT418Pair.emitSetApprovalForAll.selector) {
+        //     require(msg.sender == _pairAddress, "SFT418Pair: fallback() emitTransfers not from pair");
+
+        // }
+
+        _;
+    }
+}
+
+interface ISFT418Pair {
+    function emitTransfers(address from_, address to_, uint256[] memory tokenIds_) external;
+    function emitTransfer(address from_, address to_, uint256 tokenId_) external;
+    function emitApproval(address owner_, address operator_, uint256 tokenId_) external;
+    function emitSetApprovalForAll(address owner_, address operator_, bool approved_) external;
+}
+
+abstract contract ERC721TokenReceiver {
+    function onERC721Received(
+        address,
+        address,
+        uint256,
+        bytes calldata
+    ) external virtual returns (bytes4) {
+        return ERC721TokenReceiver.onERC721Received.selector;
+    }
 }
