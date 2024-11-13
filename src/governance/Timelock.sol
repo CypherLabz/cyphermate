ad6b0ae3691d0d12e9f1d865cbb5c836c7260b01
0xInuarashi <inuarashi.dev@gmail.com>
2024-11-13 20:26:55 +0700

    some work on timelock

--- src/governance/Timelock.sol
+++ src/governance/Timelock.sol
@@ -3,9 +3,24 @@ pragma solidity ^0.8.28;
 
 contract Timelock {
 
+    // Events
+    event AdminSet(address msgSender, address oldAdmin, address newAdmin);
+
     // Need to set admin
     address public admin;
 
+    // Constructor
+    constructor(address admin_) {
+        _setAdmin(admin_);
+    }
+
+    // Admin Handler Functions
+    function _setAdmin(address admin_) internal {
+        emit AdminSet(msg.sender, admin, admin_);
+        admin = admin_;
+    }
+
+    // Modifier for admin-only functions
     modifier onlyAdmin {
         require(msg.sender == admin, "NOT_ADMIN");
         _;
@@ -34,7 +49,7 @@ contract Timelock {
 
 
 
-
-
+    // So that accounts can send msg.value to the Timelock
+    receive() external payable {}
 
 }
